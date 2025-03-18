340d17497da82aad8ccabb0165bac94962d6473a
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-18 16:18:20 +0900

    init: 초기 설정 추가

---
+++ src/main/java/com/tilguys/matilda/MatildaApplication.java
@@ -0,0 +1,13 @@
+package com.tilguys.matilda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatildaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatildaApplication.class, args);
+    }
+
+}
