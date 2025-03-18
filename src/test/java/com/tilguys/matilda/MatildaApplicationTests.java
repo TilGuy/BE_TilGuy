192b5220391cdd246c64ae0a2a9c5fecd48e255b
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-18 16:18:20 +0900

    init: 초기 설정 추가

---
+++ src/test/java/com/tilguys/matilda/MatildaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tilguys.matilda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatildaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
