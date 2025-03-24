b0eabbad8a5fa6ac73071bc97f1c80b799604211
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-03-24 17:55:00 +0900

    feat: user controller hi 추가

--- src/main/java/com/tilguys/matilda/controller/UserController.java
+++ src/main/java/com/tilguys/matilda/controller/UserController.java
@@ -1,20 +1,21 @@
 package com.tilguys.matilda.controller;
 
-import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/api/v1/user")
-public class UserController{
+public class UserController {
 
-    public String hi(){
+    @GetMapping("/hi")
+    public String hi() {
         return "";
     }
 
-    public void signup(){
+    public void signup() {
 
     }
 
-    public void login(){
+    public void login() {
 
     }
 }
