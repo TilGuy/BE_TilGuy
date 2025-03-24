2991cc4d5b9c6255f5682109d36b80f8148e56f3
taek2222 <oot309@daum.net>
2025-03-24 23:18:56 +0900

    feat: Til CRUD 기능 추가

---
+++ src/main/java/com/tilguys/matilda/til/service/TilService.java
@@ -0,0 +1,39 @@
+package com.tilguys.matilda.til.service;
+
+import com.tilguys.matilda.til.domain.Til;
+import com.tilguys.matilda.til.dto.TilCreateRequest;
+import com.tilguys.matilda.til.dto.TilUpdateRequest;
+import com.tilguys.matilda.til.repository.TilRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TilService {
+
+    private final TilRepository tilRepository;
+
+    public Til createTil(final TilCreateRequest createRequest) {
+        Til newTil = createRequest.toEntity();
+        return tilRepository.save(newTil);
+    }
+
+    public Til getTilByTilId(final Long tilId) {
+        return tilRepository.findById(tilId)
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    public void updateTil(final TilUpdateRequest updateRequest) {
+        Til til = getTilByTilId(updateRequest.tilId());
+        til.update(updateRequest.content(), updateRequest.isPublic());
+    }
+
+    public void deleteTil(Long id) {
+        if (!tilRepository.existsById(id)) {
+            throw new IllegalArgumentException();
+        }
+        tilRepository.deleteById(id);
+    }
+}
