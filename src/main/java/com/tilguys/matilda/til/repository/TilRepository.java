bdc53c76f7fe5b6968c2e14f61687dd4b326eae4
taek2222 <oot309@daum.net>
2025-04-10 18:17:23 +0900

    feat: 날짜와 날짜 사이에 til 반환 기능 추가

--- src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
+++ src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
@@ -1,6 +1,7 @@
 package com.tilguys.matilda.til.repository;
 
 import com.tilguys.matilda.til.domain.Til;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TilRepository extends JpaRepository<Til, Long> {
     List<Til> findByUserId(final Long userId);
+
+    List<Til> findByCreatedAtBetween(final LocalDateTime from, final LocalDateTime to);
 }
