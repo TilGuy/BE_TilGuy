137f94369284b4ddcda63ed9a9d362e8b94228c3
taek2222 <oot309@daum.net>
2025-04-10 17:06:19 +0900

    refactor: 비교 시간 한국 시간 기준으로 변경

--- src/main/java/com/tilguys/matilda/global/domain/BaseEntity.java
+++ src/main/java/com/tilguys/matilda/global/domain/BaseEntity.java
@@ -3,7 +3,9 @@ package com.tilguys.matilda.global.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -20,6 +22,6 @@ public class BaseEntity {
     private LocalDateTime updatedAt;
 
     public boolean isToday() {
-        return this.createdAt.toLocalDate().equals(LocalDateTime.now().toLocalDate());
+        return this.createdAt.toLocalDate().equals(LocalDate.now(ZoneId.of("Asia/Seoul")));
     }
 }
