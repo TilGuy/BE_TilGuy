cdd47175abbcc2722fcb2e34f45285e7c43bf650
taek2222 <oot309@daum.net>
2025-04-10 16:29:32 +0900

    fix: 잘못 입력된 괄호 추가

--- src/main/java/com/tilguys/matilda/controller/AuthController.java
+++ src/main/java/com/tilguys/matilda/controller/AuthController.java
@@ -17,8 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-@RequestMapping("/api/oauth"
+@RequestMapping("/api/oauth")
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
