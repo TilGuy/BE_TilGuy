5a4178a7081677e04d7846592d338a75a9211279
황재웅 <wodnd0131@naver.com>
2025-04-11 20:06:42 +0900

    refactor: 페이지네이션 기반으로 수정

--- src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
+++ src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
@@ -3,6 +3,8 @@ package com.tilguys.matilda.til.repository;
 import com.tilguys.matilda.til.domain.Til;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,6 @@ public interface TilRepository extends JpaRepository<Til, Long> {
     List<Til> findByUserId(final Long userId);
 
     List<Til> findByCreatedAtBetween(final LocalDateTime from, final LocalDateTime to);
+
+    Page<Til> findAllByUserId(Pageable pageable, Long userId);
 }
