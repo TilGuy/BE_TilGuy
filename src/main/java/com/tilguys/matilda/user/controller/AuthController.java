3dcef2173291067fb6ae38b42abab61e2a58eb8a
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-04-20 17:02:42 +0900

    fix: 로그아웃시 삭제안되는 버그 수정
    delete시에는 Transactional 적용

--- src/main/java/com/tilguys/matilda/user/controller/AuthController.java
+++ src/main/java/com/tilguys/matilda/user/controller/AuthController.java
@@ -35,7 +35,7 @@ public class AuthController {
         Cookie jwt = new Cookie(Jwt.getCookieName(), null);
         jwt.setMaxAge(0); // 쿠키의 expiration 타임을 0으로 하여 없앤다.
         jwt.setPath("/"); // 모든 경로에서 삭제 됬음을 알린다.
-        userRefreshTokenService.deleteRefreshTokenById(id);
+        userRefreshTokenService.deleteRefreshTokenByUserId(id);
         return ResponseEntity.ok(ResponseEntity.accepted().build());
     }
 
