516a00257f7e034790910639f79f7e0acaa6f14b
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-04-20 15:13:10 +0900

    fix: 항상 jwt를 통해서 권한을 주입하도록 수정, 만료된 토큰이면 토큰 제거하도록 수정

--- src/main/java/com/tilguys/matilda/common/auth/config/PrevLoginFilter.java
+++ src/main/java/com/tilguys/matilda/common/auth/config/PrevLoginFilter.java
@@ -36,33 +36,39 @@ public class PrevLoginFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+        clearSecurityContext();
+
         Cookie[] cookies = request.getCookies();
         if (cookies == null) {
-            clearSecurityContext();
             filterChain.doFilter(request, response);
             return;
         }
 
         String token = jwt.getTokenFromCookie(cookies);
         if (token.isEmpty()) {
-            clearSecurityContext();
             filterChain.doFilter(request, response);
             return;
         }
-
         try {
             resolveJwtToken(token);
         } catch (ExpiredJwtException e) {
+            clearJwtToken(response);
             Long id = jwt.resolveUserIdWhenJwtExpired(e);
             userRefreshTokenReLogin(id, response);
         } catch (RuntimeException ignore) {
-            clearSecurityContext();
         }
         filterChain.doFilter(request, response);
     }
 
     private void clearSecurityContext() {
         SecurityContextHolder.getContext().setAuthentication(null);
+    }
+
+    private static void clearJwtToken(HttpServletResponse response) {
+        Cookie jwt = new Cookie(Jwt.getCookieName(), null);
+        jwt.setMaxAge(0); // 쿠키의 expiration 타임을 0으로 하여 없앤다.
+        jwt.setPath("/"); // 모든 경로에서 삭제 됬음을 알린다.
+        response.addCookie(jwt);
     }
 
     private void setSecurityContext(Authentication authentication) {
