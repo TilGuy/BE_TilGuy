2efbef015540a512b3380e0778e7485fa54d43e1
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-05-03 17:16:17 +0900

    feat: til tag 기능 연결

--- src/main/java/com/tilguys/matilda/til/domain/Til.java
+++ src/main/java/com/tilguys/matilda/til/domain/Til.java
@@ -1,22 +1,25 @@
 package com.tilguys.matilda.til.domain;
 
 import com.tilguys.matilda.common.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "til")
@@ -25,18 +28,22 @@ public class Til extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "til_id")
+    @Getter
     private Long tilId;
 
     @Column(name = "user_id")
     private Long userId;
 
+    @Getter
     @Column(name = "title")
     private String title;
 
+    @Getter
     @Lob
     @Column(name = "content", columnDefinition = "MEDIUMTEXT")
     private String content;
 
+    @Getter
     @Column(name = "date")
     private LocalDate date;
 
@@ -45,6 +52,11 @@ public class Til extends BaseEntity {
 
     @Column(name = "is_deleted")
     private boolean isDeleted;
+
+    @Getter
+    @OneToMany(mappedBy = "til", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Tag> tags = new ArrayList<>();
 
     public void updateContentAndVisibility(final String content, final boolean isPublic) {
         this.content = content;
@@ -60,5 +72,14 @@ public class Til extends BaseEntity {
         boolean isBeforeOrEqualTo = !date.isAfter(to);
 
         return isAfterOrEqualFrom && isBeforeOrEqualTo;
+    }
+
+    //til
+    public void updateTags(List<Tag> newTags) {
+        this.tags.clear(); // 기존 태그 삭제
+        for (Tag tag : newTags) {
+            tag.setTil(this); // 양방향 연관관계 설정
+            this.tags.add(tag);
+        }
     }
 }
