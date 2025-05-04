f67c5c07fd98c65a21db05bf803c6bc49529c97e
황재웅 <wodnd0131@naver.com>
2025-05-04 18:23:28 +0900

    feat: 같은 날짜 중복 생성 예외 처리

--- src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
+++ src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
@@ -1,6 +1,7 @@
 package com.tilguys.matilda.til.repository;
 
 import com.tilguys.matilda.til.domain.Til;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,10 +11,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TilRepository extends JpaRepository<Til, Long> {
+
     List<Til> findByUserId(final Long userId);
 
     Page<Til> findAllByUserId(final Pageable pageable, final Long userId);
 
     @Query(value = "SELECT t FROM Til t WHERE t.isDeleted = false AND t.isPublic = true ORDER BY t.createdAt DESC LIMIT 10")
     List<Til> findRecentTop10PublicTils();
+
+    boolean existsByDateAndUserId(LocalDate date, Long userId);
 }
