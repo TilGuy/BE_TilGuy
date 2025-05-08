7138b046b95c99e031f4c563fc0b4d2c40cac766
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-05-08 17:05:25 +0900

    test: 동작하지 않는 테스트 수정

--- src/test/java/com/tilguys/matilda/study/slack/SlackServiceTest.java
+++ src/test/java/com/tilguys/matilda/study/slack/SlackServiceTest.java
@@ -3,6 +3,7 @@ package com.tilguys.matilda.study.slack;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import com.tilguys.matilda.slack.service.SlackService;
+import com.tilguys.matilda.til.domain.Tag;
 import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -21,10 +22,13 @@ class SlackServiceTest {
 
     @Test
     void 설정_파일에_등록된_채널로_슬랙_알람을_보낼수_있다() {
+        List<Tag> tags = List.of(new Tag(null, "카프카", null), new Tag(null, "객체지향", null));
+
         assertDoesNotThrow(() -> slackService.sendTilWriteAlarm(
                 "오늘은 Kafka를 공부했다.",
                 "praisebak -> 투다로 뜨게할 방법이 없을까 - config에 nickname 넣자!",
                 "2021-01-24 월요일 - 이부분은 컨트롤러에서 전달해줄것 - ",
-                List.of("카프카", "객체지향", "자바")));
+                tags
+        ));
     }
 }
