de10b88262fa58daaf6eebaa9b5ec19ac94f0cbe
황재웅 <wodnd0131@naver.com>
2025-05-16 11:14:38 +0900

    refactor: JPQL 방식 대신 JPA로 중복 검증 수정

--- src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
+++ src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
@@ -1,14 +1,14 @@
 package com.tilguys.matilda.til.repository;
 
 import com.tilguys.matilda.til.domain.Til;
-import java.time.LocalDate;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface TilRepository extends JpaRepository<Til, Long> {
@@ -20,6 +20,5 @@ public interface TilRepository extends JpaRepository<Til, Long> {
     @Query(value = "SELECT t FROM Til t WHERE t.isDeleted = false AND t.isPublic = true ORDER BY t.createdAt DESC LIMIT 10")
     List<Til> findRecentPublicTils();
 
-    @Query(value = "SELECT CASE WHEN COUNT(t) > 0 THEN true ELSE false END FROM Til t WHERE t.isDeleted = false AND t.date = :date AND t.userId = :userId")
-    boolean existsByDateAndUserId(@Param("date") LocalDate date, @Param("userId") Long userId);
+    boolean existsByDateAndUserIdAndIsDeletedFalse(LocalDate date, Long userId);
 }
