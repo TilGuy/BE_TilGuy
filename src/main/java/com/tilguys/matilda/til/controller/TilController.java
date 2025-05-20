768619f40fedca29caec20c8f7ee4cdc0776d80f
황재웅 <wodnd0131@naver.com>
2025-05-20 21:15:12 +0900

    refactor: Til 수정 시 날짜 변경 추가

--- src/main/java/com/tilguys/matilda/til/controller/TilController.java
+++ src/main/java/com/tilguys/matilda/til/controller/TilController.java
@@ -10,21 +10,22 @@ import com.tilguys.matilda.til.dto.TilDetailsResponse;
 import com.tilguys.matilda.til.dto.TilUpdateRequest;
 import com.tilguys.matilda.til.service.RecentTilService;
 import com.tilguys.matilda.til.service.TilService;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @RequestMapping("/api/til")
 @RestController
@@ -51,9 +52,12 @@ public class TilController {
         return ResponseEntity.noContent().build();
     }
 
-    @PatchMapping
-    public ResponseEntity<?> updateTil(@RequestBody final TilUpdateRequest request) {
-        tilService.updateTil(request);
+    @PutMapping("/{tilId}")
+    public ResponseEntity<?> updateTil(
+            @PathVariable final Long tilId,
+            @RequestBody final TilUpdateRequest request
+    ) {
+        tilService.updateTil(tilId, request);
         return ResponseEntity.ok().build();
     }
 
