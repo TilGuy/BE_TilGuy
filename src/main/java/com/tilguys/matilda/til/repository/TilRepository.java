661e4aa9694d9dd574c0ecc977e89fa33a119b96
taek2222 <oot309@daum.net>
2025-05-25 12:42:54 +0900

    fix: TilRepository 메서드 네이밍 수정 및 TilService에서 반영

--- src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
+++ src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
@@ -1,13 +1,12 @@
 package com.tilguys.matilda.til.repository;
 
 import com.tilguys.matilda.til.domain.Til;
+import java.time.LocalDate;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Repository
 public interface TilRepository extends JpaRepository<Til, Long> {
@@ -20,5 +19,5 @@ public interface TilRepository extends JpaRepository<Til, Long> {
 
     List<Til> findTop10ByIsDeletedFalseAndIsPublicTrueOrderByCreatedAtDesc();
 
-    boolean existsByDateAndUserIdAndIsDeletedFalse(LocalDate date, Long userId);
+    boolean existsByDateAndTilUserIdAndIsDeletedFalse(LocalDate date, Long userId);
 }
