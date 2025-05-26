c04fe6688066cc1a21f80bd7adb8bb201518b91c
taek2222 <oot309@daum.net>
2025-05-26 22:52:35 +0900

    test: 통합 및 유닛 테스트 개선
    - `@ActiveProfiles("test")` 추가로 `TilServiceIntegrationTest` 환경 프로파일 설정
    - `RecentTilServiceTest`에서 페이징 처리 로직 반영
      - `findTop10By...`에서 `findAllBy...`로 리포지토리 메서드 수정
      - `PageRequest` 및 `Page` 객체 활용
    - `TilBookmarkServiceTest`의 `Til` 생성 시 `member.getId()` 대신 `member`로 설정

--- src/test/java/com/tilguys/matilda/til/service/TilServiceIntegrationTest.java
+++ src/test/java/com/tilguys/matilda/til/service/TilServiceIntegrationTest.java
@@ -13,7 +13,9 @@ import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class TilServiceIntegrationTest {
 
