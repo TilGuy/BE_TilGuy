1f2910e793918ae891fe7ee5128787506266313c
황재웅 <wodnd0131@naver.com>
2025-06-01 14:44:36 +0900

    feat: Til create 시 Reference 저장

--- src/test/java/com/tilguys/matilda/bookmark/service/TilBookmarkServiceTest.java
+++ src/test/java/com/tilguys/matilda/bookmark/service/TilBookmarkServiceTest.java
@@ -53,7 +53,8 @@ class TilBookmarkServiceTest {
     @Test
     void toggleBookmark() {
         TilUser member = new TilUser(null, ProviderInfo.GITHUB, "asdf", Role.USER, "asdf", "asdfasdf");
-        Til til = new Til(null, member.getId(), "Asdf", "asdf", LocalDate.now(), true, false, new ArrayList<>());
+        Til til = new Til(null, member, "Asdf", "asdf", LocalDate.now(),
+                true, false, new ArrayList<>(), new ArrayList<>());
         em.persist(member);
         em.persist(til);
         em.flush();
@@ -74,7 +75,8 @@ class TilBookmarkServiceTest {
     @Test
     void removeBookmark() {
         TilUser member = new TilUser(null, ProviderInfo.GITHUB, "asdf", Role.USER, "asdf", "asdfasdf");
-        Til til = new Til(null, member.getId(), "Asdf", "asdf", LocalDate.now(), true, false, new ArrayList<>());
+        Til til = new Til(null, member, "Asdf", "asdf", LocalDate.now(),
+                true, false, new ArrayList<>(), new ArrayList<>());
         em.persist(member);
         em.persist(til);
         em.flush();
