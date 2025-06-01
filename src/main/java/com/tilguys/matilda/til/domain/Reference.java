b07322b328b74bb9c496d630ef90b00306a85bd0
황재웅 <wodnd0131@naver.com>
2025-06-01 14:27:46 +0900

    feat: Reference Entity 추가

---
+++ src/main/java/com/tilguys/matilda/til/domain/Reference.java
@@ -0,0 +1,43 @@
+package com.tilguys.matilda.til.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reference {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Getter
+    @Column(name = "word")
+    private String word;
+
+    @NotNull
+    @Getter
+    @Column(name = "reference", columnDefinition = "TEXT")
+    private String reference;
+
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "til_id")
+    private Til til;
+
+}
+
