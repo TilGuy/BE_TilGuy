2d1a7bd67f612c7e6540b992c6377655fb3cdaa3
황재웅 <wodnd0131@naver.com>
2025-06-01 15:31:22 +0900

    feat: Til 조회 시 Reference 조회

--- src/main/java/com/tilguys/matilda/til/dto/TilDetailResponse.java
+++ src/main/java/com/tilguys/matilda/til/dto/TilDetailResponse.java
@@ -1,25 +1,22 @@
 package com.tilguys.matilda.til.dto;
 
-import com.tilguys.matilda.til.domain.Tag;
 import com.tilguys.matilda.til.domain.Til;
-import java.util.List;
 
 public record TilDetailResponse(
         Long tilId,
         String title,
         String content,
-        List<String> tags
+        TagsResponse tags,
+        ReferencesResponse references
 ) {
 
     public static TilDetailResponse fromEntity(Til til) {
-        List<String> tags = til.getTags().stream()
-                .map(Tag::getTagString)
-                .toList();
         return new TilDetailResponse(
                 til.getTilId(),
                 til.getTitle(),
                 til.getContent(),
-                tags
+                new TagsResponse(til.getTags()),
+                new ReferencesResponse(til.getReferences())
         );
     }
 }
