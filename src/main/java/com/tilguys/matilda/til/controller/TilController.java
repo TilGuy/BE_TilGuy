b8ec0cca63e0360a0f095bf2d692cf7d079fa79d
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-06-15 21:44:20 +0900

    refactor: 태그 생성 로깅 및 핵심 태그 프롬프트 수정

--- src/main/java/com/tilguys/matilda/til/controller/TilController.java
+++ src/main/java/com/tilguys/matilda/til/controller/TilController.java
@@ -15,6 +15,7 @@ import com.tilguys.matilda.til.service.TilService;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -28,6 +29,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/api/til")
 @RestController
 @RequiredArgsConstructor
