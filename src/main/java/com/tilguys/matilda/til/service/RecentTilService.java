e6ffafd8201b60e830c2bb6194b136d8586cf654
taek2222 <oot309@daum.net>
2025-06-19 23:50:37 +0900

    feat: 트랜잭션 추가

--- src/main/java/com/tilguys/matilda/til/service/RecentTilService.java
+++ src/main/java/com/tilguys/matilda/til/service/RecentTilService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +17,7 @@ public class RecentTilService {
 
     private final TilRepository tilRepository;
 
+    @Transactional(readOnly = true)
     public List<TilWithUserResponse> getRecentTils() {
         PageRequest pageRequest = PageRequest.of(0, 10, Sort.Direction.DESC, "createdAt");
         Page<Til> recentTils = tilRepository.findAllByIsPublicTrueAndIsDeletedFalse(pageRequest);
