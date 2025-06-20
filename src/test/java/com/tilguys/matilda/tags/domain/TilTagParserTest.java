d60b73028a3be566f97ca71377fbda2a91764430
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-06-20 17:25:10 +0900

    test: TilTagParserTest 추가

---
+++ src/test/java/com/tilguys/matilda/tags/domain/TilTagParserTest.java
@@ -0,0 +1,28 @@
+package com.tilguys.matilda.tags.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import com.tilguys.matilda.tag.domain.TilTagParser;
+import com.tilguys.matilda.tag.domain.TilTags;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SpringBootTest
+@ActiveProfiles("test")
+class TilTagParserTest {
+
+    @Test
+    void 요청이_json이_아니면_유효하지_않으면_예외가_발생한다() {
+        TilTagParser tilTagParser = new TilTagParser();
+        Assertions.assertAll(
+                () -> assertThatThrownBy(() -> tilTagParser.parseTags("not json")),
+                () -> assertThatThrownBy(() -> tilTagParser.parseSubTags("not json", new TilTags(new ArrayList<>())))
+        );
+    }
+}
