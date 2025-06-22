e1b31508d805a9b73d0213c33e76d31e0ab1fda2
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-06-22 20:59:31 +0900

    refactor: 피드백 리팩터링

--- src/test/java/com/tilguys/matilda/tags/domain/TilTagParserTest.java
+++ src/test/java/com/tilguys/matilda/tags/domain/TilTagParserTest.java
@@ -21,8 +21,9 @@ class TilTagParserTest {
     void 요청이_json이_아니면_유효하지_않으면_예외가_발생한다() {
         TilTagParser tilTagParser = new TilTagParser();
         Assertions.assertAll(
-                () -> assertThatThrownBy(() -> tilTagParser.parseTags("not json")),
-                () -> assertThatThrownBy(() -> tilTagParser.parseSubTags("not json", new TilTags(new ArrayList<>())))
+                () -> assertThatThrownBy(() -> tilTagParser.parseTags("not json")).doesNotThrowAnyException(),
+                () -> assertThatThrownBy(() -> tilTagParser.parseSubTags("not json",
+                        new TilTags(new ArrayList<>()))).doesNotThrowAnyException()
         );
     }
 }
