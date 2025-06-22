0b035bcc888af3b475054e3742f264c5d0101d05
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-06-22 21:04:18 +0900

    refactor: 피드백 리팩터링

--- src/test/java/com/tilguys/matilda/til/service/TilServiceTest.java
+++ src/test/java/com/tilguys/matilda/til/service/TilServiceTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import com.tilguys.matilda.til.domain.Til;
-import com.tilguys.matilda.til.dto.TilDetailResponse;
 import com.tilguys.matilda.til.dto.TilDetailsResponse;
 import com.tilguys.matilda.til.repository.TilRepository;
 import java.time.LocalDate;
@@ -43,8 +42,6 @@ class TilServiceTest {
         // when
         TilDetailsResponse result = tilService.getTilByDateRange(userId, from, to);
 
-        System.out.println(TilDetailResponse.fromEntity(withinRange));
-        System.out.println(result.tils());
         // then
         assertThat(result.tils())
                 .hasSize(1);
