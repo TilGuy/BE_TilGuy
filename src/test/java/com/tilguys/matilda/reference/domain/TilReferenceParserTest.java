298b57b34100291974ac8156aed782f34884c3cf
황재웅 <wodnd0131@naver.com>
2025-06-23 23:13:17 +0900

    refactor: parseReferences() 예외 메세지 수정

--- src/test/java/com/tilguys/matilda/reference/domain/TilReferenceParserTest.java
+++ src/test/java/com/tilguys/matilda/reference/domain/TilReferenceParserTest.java
@@ -1,6 +1,7 @@
 package com.tilguys.matilda.reference.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tilguys.matilda.common.external.exception.OpenAIException;
@@ -30,8 +31,13 @@ class TilReferenceParserTest {
     @Test
     void 요청이_null이면_예외가_발생한다() {
         TilReferenceParser tilReferenceParser = new TilReferenceParser(objectMapper);
-        assertThatThrownBy(() -> tilReferenceParser.parseReferences(null))
-                .isInstanceOf(OpenAIException.class)
-                .hasMessageContaining("Response JSON is null or empty");
+        assertAll(
+                () -> assertThatThrownBy(() -> tilReferenceParser.parseReferences(null))
+                        .isInstanceOf(OpenAIException.class)
+                        .hasMessageContaining("Response JSON is null or empty"),
+                () -> assertThatThrownBy(() -> tilReferenceParser.parseReferences(""))
+                        .isInstanceOf(OpenAIException.class)
+                        .hasMessageContaining("Response JSON is null or empty")
+        );
     }
 }
