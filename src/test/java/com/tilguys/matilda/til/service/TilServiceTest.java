94aa04c2ff333ada669a429d3e4a9cca48164bcd
taek2222 <oot309@daum.net>
2025-06-25 00:17:52 +0900

    refactor: @Nested 적용 및 DTO 수정

--- src/test/java/com/tilguys/matilda/til/service/TilServiceTest.java
+++ src/test/java/com/tilguys/matilda/til/service/TilServiceTest.java
@@ -4,9 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.tilguys.matilda.til.domain.Til;
-import com.tilguys.matilda.til.dto.TilUpdateRequest;
+import com.tilguys.matilda.til.dto.TilDefinitionRequest;
 import com.tilguys.matilda.til.repository.TilRepository;
 import java.time.LocalDate;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,56 +21,63 @@ class TilServiceTest {
     @Autowired
     private TilRepository tilRepository;
 
-    @Test
-    void 정상적으로_TIL이_업데이트된다() {
-        // given
-        Til til = createTestTilFixture();
-        til = tilRepository.save(til);
+    @Nested
+    class TIL_업데이트_테스트 {
 
-        TilUpdateRequest request = new TilUpdateRequest(
-                "new content", LocalDate.of(2024, 6, 2), true, "new title"
-        );
+        @Test
+        void 정상적으로_TIL이_업데이트된다() {
+            // given
+            long userId = 1L;
+            Til til = createTestTilFixture();
+            til = tilRepository.save(til);
 
-        // when
-        tilService.updateTil(til.getTilId(), request);
+            TilDefinitionRequest request = new TilDefinitionRequest(
+                    "new title", "new content", LocalDate.of(2024, 6, 2), true
+            );
 
-        // then
-        Til updated = tilRepository.findById(til.getTilId()).orElseThrow();
-        assertThat(updated.getContent()).isEqualTo("new content");
-        assertThat(updated.isPublic()).isTrue();
-        assertThat(updated.getDate()).isEqualTo(LocalDate.of(2024, 6, 2));
-        assertThat(updated.getTitle()).isEqualTo("new title");
-    }
+            // when
+            tilService.updateTil(til.getTilId(), request, userId);
 
-    @Test
-    void 존재하지_않는_TIL을_업데이트하면_예외가_발생한다() {
-        // given
-        Long notExistId = 9999L;
-        TilUpdateRequest request = new TilUpdateRequest(
-                "content", LocalDate.now(), true, "title"
-        );
+            // then
+            Til updated = tilRepository.findById(til.getTilId()).orElseThrow();
+            assertThat(updated.getContent()).isEqualTo("new content");
+            assertThat(updated.isPublic()).isTrue();
+            assertThat(updated.getDate()).isEqualTo(LocalDate.of(2024, 6, 2));
+            assertThat(updated.getTitle()).isEqualTo("new title");
+        }
 
-        // when & then
-        assertThatThrownBy(() -> tilService.updateTil(notExistId, request))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @Test
+        void 존재하지_않는_TIL을_업데이트하면_예외가_발생한다() {
+            // given
+            long notExistId = 9999L;
+            long userId = 1L;
+            TilDefinitionRequest request = new TilDefinitionRequest(
+                    "title", "content", LocalDate.now(), true
+            );
 
-    @Test
-    void 삭제된_TIL은_업데이트되지_않는다() {
-        // given
-        Til til = createTestTilFixture();
+            // when && then
+            assertThatThrownBy(() -> tilService.updateTil(notExistId, request, userId))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-        til.markAsDeleted();
-        tilRepository.save(til);
+        @Test
+        void 삭제된_TIL은_업데이트되지_않는다() {
+            // given
+            Til til = createTestTilFixture();
+            long userId = 1L;
 
-        TilUpdateRequest request = new TilUpdateRequest(
-                "new content", LocalDate.of(2024, 6, 2), true, "new title"
-        );
+            til.markAsDeleted();
+            tilRepository.save(til);
 
-        // when && then
-        assertThatThrownBy(() -> tilService.updateTil(til.getTilId(), request))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("삭제된 TIL은 수정할 수 없습니다.");
+            TilDefinitionRequest request = new TilDefinitionRequest(
+                    "new title", "new content", LocalDate.of(2024, 6, 2), true
+            );
+
+            // when && then
+            assertThatThrownBy(() -> tilService.updateTil(til.getTilId(), request, userId))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("삭제된 TIL은 수정할 수 없습니다.");
+        }
     }
 
     private Til createTestTilFixture() {
