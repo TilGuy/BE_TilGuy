6259127f15f0ad6818e781869f11624669018140
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-06-25 17:58:03 +0900

    performance: N+1 문제 해결

--- src/main/java/com/tilguys/matilda/tag/repository/TagRepository.java
+++ src/main/java/com/tilguys/matilda/tag/repository/TagRepository.java
@@ -4,10 +4,17 @@ import com.tilguys.matilda.til.domain.Tag;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-    List<Tag> findByCreatedAtGreaterThanEqual(LocalDateTime recent);
+    @Query("""
+                SELECT t FROM Tag t
+                JOIN FETCH t.til
+                WHERE t.createdAt >= :start
+            """)
+    List<Tag> findByCreatedAtGreaterThanEqual(@Param("start") LocalDateTime recent);
 }
