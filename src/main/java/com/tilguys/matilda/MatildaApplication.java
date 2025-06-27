830d664af095e3a02a00c5be0f161ebcfb4663ca
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-06-28 02:35:02 +0900

    hotfix: 스케줄링 적용안됨 (로컬에서는 수동으로 동작시키는 방법이 있어 해당 방법 사용했었음)

--- src/main/java/com/tilguys/matilda/MatildaApplication.java
+++ src/main/java/com/tilguys/matilda/MatildaApplication.java
@@ -3,9 +3,11 @@ package com.tilguys.matilda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class MatildaApplication {
 
     public static void main(String[] args) {
