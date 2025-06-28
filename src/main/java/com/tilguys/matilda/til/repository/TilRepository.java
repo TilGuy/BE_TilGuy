091511bcceec5577d42eaccf7fdad217525791a3
황재웅 <wodnd0131@naver.com>
2025-06-29 00:20:05 +0900

    hotfix: 배치 설정 추가

--- src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
+++ src/main/java/com/tilguys/matilda/til/repository/TilRepository.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TilRepository extends JpaRepository<Til, Long> {
 
+    @EntityGraph(attributePaths = {"tilUser"})
     Page<Til> findAllByIsPublicTrueAndIsDeletedFalse(final Pageable pageable);
 
     List<Til> findByTilUserId(final Long userId);
@@ -25,6 +27,7 @@ public interface TilRepository extends JpaRepository<Til, Long> {
 
     List<Til> findByCreatedAtGreaterThanEqual(LocalDateTime recent);
 
+    @EntityGraph(attributePaths = {"tilUser"})
     @Query("SELECT t FROM Til t " +
             "WHERE t.isDeleted = false AND t.isPublic = true " +
             "AND (:cursorDate IS NULL OR t.createdAt < :cursorDate " +
