df9223bc19090ba267dcd875bf53fe4ab2670d36
taek2222 <oot309@daum.net>
2025-06-29 16:50:51 +0900

    test: ReadOnly 트랜잭션 삭제

--- src/main/java/com/tilguys/matilda/til/service/TilService.java
+++ src/main/java/com/tilguys/matilda/til/service/TilService.java
@@ -114,7 +114,6 @@ public class TilService {
                 .toList();
     }
 
-    @Transactional(readOnly = true)
     public TilDetailsResponse getTilByDateRange(final Long userId, final LocalDate from, final LocalDate to) {
         List<Til> tils = tilRepository.findAllByTilUserIdAndDateBetweenAndIsDeleted(userId, from, to, false);
 
