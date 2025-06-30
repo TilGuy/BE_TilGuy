400d1dbac2874e7a2cfc0f45a78a941ecf9bfb69
taek2222 <oot309@daum.net>
2025-06-30 15:34:29 +0900

    fix: CORS 에러 개선

--- src/main/java/com/tilguys/matilda/common/auth/config/SecurityConfig.java
+++ src/main/java/com/tilguys/matilda/common/auth/config/SecurityConfig.java
@@ -87,7 +87,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(frontendUrl, "http://matilda.woowacourse.com")
+                .allowedOrigins(frontendUrl, "https://matilda.woowacourse.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type")
                 .allowCredentials(true)
