f0e6e13191f807a535351cf08e93ab336532bc12
taek2222 <oot309@daum.net>
2025-07-02 20:06:00 +0900

    feat: GitHubUpload 객체 생성

---
+++ src/main/java/com/tilguys/matilda/github/GitHubUpload.java
@@ -0,0 +1,55 @@
+package com.tilguys.matilda.github;
+
+import java.time.LocalDate;
+import lombok.Getter;
+
+public class GitHubUpload {
+
+    private static final String COMMIT_MESSAGE_PREFIX = "[TIL]: ";
+    private static final String GITHUB_API_URL_FORMAT = "https://api.github.com/repos/%s/%s/contents/til/%s.md";
+    private static final String CONTENT_TEMPLATE = """
+            ### 🔗 [원본 URL](https://matilda.woowacourse.com/api/til/share/%d)
+            
+            ---
+            
+            # %s
+            
+            %s
+            
+            """;
+
+    @Getter
+    private final String accessToken;
+
+    @Getter
+    private final String uploadsUrl;
+
+    @Getter
+    private final String contents;
+
+    @Getter
+    private final String commitMessage;
+
+    public GitHubUpload(String accessToken, Long tilId, String identifier, String repositoryName, String title,
+                        LocalDate date,
+                        String contents) {
+        this.accessToken = accessToken;
+        this.uploadsUrl = generateUploadUrl(identifier, repositoryName, date);
+        this.contents = generateContents(tilId, title, contents);
+        this.commitMessage = COMMIT_MESSAGE_PREFIX + title;
+    }
+
+    public String generateUploadUrl(String identifier, String repositoryName, LocalDate date) {
+        return String.format(GITHUB_API_URL_FORMAT,
+                identifier,
+                repositoryName,
+                date);
+    }
+
+    public String generateContents(Long tilId, String title, String contents) {
+        return String.format(CONTENT_TEMPLATE,
+                tilId,
+                title,
+                contents);
+    }
+}
