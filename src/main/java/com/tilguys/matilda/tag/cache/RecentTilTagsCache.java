04adb7b3e3a2a612f3db6c7b58f040a2a43249ef
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-08-26 22:25:15 +0900

    refactor: redis auto configuration 친화 코드로 수정

--- src/main/java/com/tilguys/matilda/tag/cache/RecentTilTagsCache.java
+++ src/main/java/com/tilguys/matilda/tag/cache/RecentTilTagsCache.java
@@ -1,33 +1,32 @@
 package com.tilguys.matilda.tag.cache;
 
 import com.tilguys.matilda.common.auth.exception.MatildaException;
-import com.tilguys.matilda.common.cache.RedisCacheService;
 import com.tilguys.matilda.tag.domain.TilTagRelations;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Component;
-
-import java.time.Duration;
-import java.util.logging.Logger;
 
 @Slf4j
 @Component
 public class RecentTilTagsCache {
 
     private static final String RECENT_TAG_RELATIONS_KEY = "recent:til:relations";
-    private static final Duration RECENT_TAG_RELATIONS_TTL = Duration.ofMinutes(40);
-    private static final Logger logger = Logger.getLogger(RecentTilTagsCache.class.getName());
 
-    private final RedisCacheService redisCacheService;
+    private final Cache cache;
     private TilTagRelations recentTagRelations;
 
-    public RecentTilTagsCache(RedisCacheService redisCacheService) {
-        this.redisCacheService = redisCacheService;
+    public RecentTilTagsCache(CacheManager cacheManager) {
+        this.cache = cacheManager.getCache("tilTags");
     }
 
     public TilTagRelations getRecentTagRelations() {
         try {
-            return redisCacheService.get(RECENT_TAG_RELATIONS_KEY, TilTagRelations.class)
-                    .orElseThrow(() -> new MatildaException("캐시를 가져오는데 실패하였습니다"));
+            TilTagRelations cached = cache.get(RECENT_TAG_RELATIONS_KEY, TilTagRelations.class);
+            if (cached == null) {
+                throw new MatildaException("캐시를 가져오는데 실패하였습니다");
+            }
+            return cached;
         } catch (Exception e) {
             log.error("최근 태그 정보를 가져오는데 실패하였습니다", e);
             return recentTagRelations;
@@ -36,6 +35,6 @@ public class RecentTilTagsCache {
 
     public void updateRecentTagRelations(TilTagRelations recentTagRelations) {
         this.recentTagRelations = recentTagRelations;
-        redisCacheService.set(RECENT_TAG_RELATIONS_KEY, recentTagRelations, RECENT_TAG_RELATIONS_TTL);
+        cache.put(RECENT_TAG_RELATIONS_KEY, recentTagRelations);
     }
 }
